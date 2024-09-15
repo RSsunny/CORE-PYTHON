810c71fc5249d6b3e77eb6ef747afb56a5b04779
RSsunny <sunnysharif154@gmail.com>
2024-09-15 19:27:41 +0600

    first commit

---
+++ pythonProject/dataTypes.py
@@ -0,0 +1,34 @@
+# Data Types
+
+# string
+myName="Rabius Sunny"
+# print(type(myName))
+#---------------------
+# number
+x=10
+# y="15"
+# print("type of x: ",type(x))
+# sum=x+int(y)
+# print(sum)
+# -------------------------
+# float number
+money=10.5
+# print(type(money))
+# --------------------------
+# complex number
+matrix=10j
+# print(type(matrix))
+#---------------------------
+# list
+student= ["Hasib", "Ashik", "Juwel", "Saifull"]
+# student.append("Emon")
+# student.remove("Juwel")
+# student.pop()
+# student.pop(2)
+# student.insert(1,"Arif")
+# print(student)
+# print(type(student))
+# --------------------------------
+# Tuple (immutable)
+bracketsName=("Parentheses","Curly Brackets","Square Brackets","Angle Brackets")
+print(type(bracketsName))
